7f112f9039273d1951ec38a532432f74c52991f7
unknown <maja.geo21@gmail.com>
2022-04-12 08:17:27 +0200

    username validator

--- Backend/middleware/validInfo.js
+++ Backend/middleware/validInfo.js
@@ -1,31 +1,31 @@
-// module.exports = function (req, res, next) {
+module.exports = function (req, res, next) {
 
-//     const { username, password } = req.body;
+    const { username, password } = req.body;
 
-//     function validUsername(adminUsername) {
-//         return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(adminUsername);
-//     }
+    function validUsername(adminUsername) {
+        return /^(?=.{4,20}$)(?:[a-zA-Z\d]+(?:(?:\.|-|_)[a-zA-Z\d])*)+$/.test(adminUsername);
+    }
 
-//     if (req.path === "/register") {
+    if (req.path === "/register") {
 
-//         console.log(!username.length);
+        console.log(!username.length);
 
-//         if (![username, password].every(Boolean)) {
-//             return res.status(401).json("Missing Credentials");
+        if (![username, password].every(Boolean)) {
+            return res.status(401).json("Missing Credentials");
 
-//         } else if (!validUsername(username)) {
-//             return res.status(401).json("Invalid Username");
+        } else if (!validUsername(username)) {
+            return res.status(401).json("Invalid Username");
 
-//         }
-//     } else if (req.path === "/login") {
+        }
+    } else if (req.path === "/login") {
 
-//         if (![username, password].every(Boolean)) {
-//             return res.status(401).json("Missing Credentials");
+        if (![username, password].every(Boolean)) {
+            return res.status(401).json("Missing Credentials");
 
-//         } else if (!validUsername(username)) {
-//             return res.status(401).json("Invalid Username");
-//         }
-//     }
+        } else if (!validUsername(username)) {
+            return res.status(401).json("Invalid Username");
+        }
+    }
 
-//     next();
-// };
+    next();
+};
