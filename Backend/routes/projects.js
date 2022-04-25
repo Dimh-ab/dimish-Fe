357bd1d0f4e42ef136a6beb16dfdf27542b96df3
inga0101 <ingaa134@gmail.com>
2022-04-25 16:52:52 +0200

    cleaned up and added missing variable to edit route

--- Backend/routes/projects.js
+++ Backend/routes/projects.js
@@ -6,8 +6,8 @@ const authorize = require('../middleware/authorization');
 //GET ALL PROJECTS
 router.get('/', async (req, res) => {
     try {
-        const project = await client.query("SELECT title, description, picture, category, image_url, id FROM projects")
-                                
+        const project = await client.query("SELECT title, description, picture, category, image_url, id FROM projects") 
+                                    
         res.json(project.rows);
 
     } catch (error) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const project = await client.query("SELECT id, title, description, picture, category, image_url FROM projects WHERE id = $1", [id]);
+        const project = await client.query("SELECT title, description, picture, category, id, image_url FROM projects WHERE id = $1", [id]);
 
         res.json(project.rows[0]);
 
@@ -70,7 +70,7 @@ router.put('/:id', authorize, async (req, res) => {
         const { id } = req.params;
         const { title, description, picture, image_url, category } = req.body;
 
-        const project = await client.query("UPDATE projects SET id = $1, title = $2, description = $3, picture = $4, image_url = $5 category = $6 WHERE id = $1 RETURNING *", [title, description, picture, image_url, category, id]);
+        const project = await client.query("UPDATE projects SET title = $1, description = $2, picture = $3, image_url = $4, category = $5 WHERE id = $6 RETURNING *", [title, description, picture, image_url, category, id]);
 
         res.json(project.rows[0]);
         
