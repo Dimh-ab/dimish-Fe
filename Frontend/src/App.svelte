b4d20735ef713377156e647dddf8db0e0242eb58
unknown <maja.geo21@gmail.com>
2022-05-06 09:53:10 +0200

    homepage update

--- Frontend/src/App.svelte
+++ Frontend/src/App.svelte
@@ -10,27 +10,44 @@
 	export let url = "";
 </script>
 
-<Router {url}>
-	<nav>
-		<Link to="/">Home</Link>
-		<Link to="login">Login</Link>
-		<Link to="library">Library</Link>
-		<Link to="dashboard">Dashboard</Link>
-	</nav>
-	<Route path="/">
-		<Home />
-	</Route>
+<body >
+	
+	<Router {url}>
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="login">Login</Link>
+			<Link to="library">Library</Link>
+			<Link to="dashboard">Dashboard</Link>
+		</nav>
+		
+		<a href="https://www.di-mh.com/">dimh</a>
 
-	<Route path="login" >
-		<Login />
-	</Route>
 
-	<Route path="library" >
-		<Library />
-	</Route>
+		<Route path="/">
+			<Home />
+		</Route>
+	
+		<Route path="login" >
+			<Login />
+		</Route>
+	
+		<Route path="library" >
+			<Library />
+		</Route>
+	
+		<ProtectedRoute path="dashboard" component={Dashboard} />
+	</Router>
 
-	<ProtectedRoute path="dashboard" component={Dashboard} />
-</Router>
+</body>
+
 
 <style>
+
+	a {
+		font-size: 2rem;
+		color: rgb(240, 167, 9);
+		text-decoration: none;
+	}
+
+
 </style>
--- Frontend/src/pages/Home.svelte
+++ Frontend/src/pages/Home.svelte
@@ -1,7 +1,57 @@
 <script>
+    import { navigate } from "svelte-routing";
+    import { fade } from "svelte/transition";
+    //let wasClicked = false;
 
+    const zoom = (node, scale = 1.5) => {
+        node.style.transition = "3s";
+
+        function zoomIn() {
+            node.style.transform = `scale(${scale})`;
+            node.style.opacity = 0;
+
+            setTimeout(function () {
+                navigate("/library");
+            }, 3000);
+        }
+
+        node.addEventListener("click", zoomIn);
+
+        console.log("clicked");
+    };
 </script>
 
-<div>
-    <h1>Welcome to the library</h1>
+<div class="sprite">
+    <img class="girl" src="/static/images/girl2.png" alt="girl" />
+    <img
+        class="door"
+        use:zoom={10}
+        src="/static/images/book-door.png"
+        alt="book-door"
+    />
 </div>
+
+<style>
+    .sprite {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 3vh;
+    }
+
+    .door {
+        width: 450px;
+        height: auto;
+        cursor: pointer;
+    }
+
+    .girl {
+        width: 200px;
+        height: auto;
+        position: absolute;
+        top: 60vh;
+        left: 40vw;
+        z-index: 2;
+    }
+
+</style>
