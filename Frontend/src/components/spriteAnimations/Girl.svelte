fc21cc88b6d21021b2646dea4ea04f24890500bf
unknown <maja.geo21@gmail.com>
2022-10-31 11:09:21 +0100

    scroll bug fixed

--- Frontend/src/components/spriteAnimations/Girl.svelte
+++ Frontend/src/components/spriteAnimations/Girl.svelte
@@ -1,18 +1,16 @@
 <script>
-
-    import { allKidsBooksRead} from "../../stores";
-    import { fade } from 'svelte/transition';
+    import { allKidsBooksRead } from "../../stores";
+    import { fade } from "svelte/transition";
     import { onMount } from "svelte";
 
     let key = "";
-    let girlTransform = 0
+    let girlTransform = 0;
 
     // TODO:
     // - add smoke sprite animation when transforming character
     // - girl transforms after placing down last book in category
 
-
-     //ver1
+    //ver1
 
     // window.addEventListener("load", () => {
     //     const canvas = document.getElementById("canvas1");
@@ -195,10 +193,10 @@
     //ver2
 
     // so it loads the girl the first time you go to library
-    onMount(() => avatarSpriteAnimation())
+    onMount(() => avatarSpriteAnimation());
 
     // instead of window.addEventlistener('load' () => {})
-    // the avatarSpriteAnimation function is connected to <svelte:window /> 
+    // the avatarSpriteAnimation function is connected to <svelte:window />
     const avatarSpriteAnimation = () => {
         const canvas = document.getElementById("canvas1");
         // console.log(canvas);
@@ -240,18 +238,46 @@
                     console.log(e.key, this.keys);
                 });
 
-                window.addEventListener("wheel", (e) => {
-                    // console.log(e);
-                    if (
-                        e.deltaY === 100 ||
-                        (e.deltaY === -100 &&
-                            this.keys.indexOf(e.deltaY) === -1)
-                    ) {
-                        this.keys.push(e.deltaY);
-                    } else {
-                       
-                    }
-                    // console.log(e.deltaY, this.keys);
+                // window.addEventListener("wheel", (e) => {
+                //     // console.log(e);
+                //     if (
+                //         e.deltaY === 100 ||
+                //         (e.deltaY === -100 &&
+                //             this.keys.indexOf(e.deltaY) === -1)
+                //     ) {
+                //         this.keys.push(e.deltaY);
+                //     } else {
+
+                //     }
+                //     console.log(e.deltaY, this.keys);
+                // });
+
+                const onScrollStop = (callback) => {
+                    let isScrolling;
+                    window.addEventListener(
+                        "wheel",
+                        (e) => {
+                            clearTimeout(isScrolling);
+                            isScrolling = setTimeout(() => {
+                                callback();
+                            }, 150);
+                            if (
+                                e.deltaY === 100 ||
+                                (e.deltaY === -100 &&
+                                    this.keys.indexOf(e.deltaY) === -1)
+                            ) {
+                                this.keys.push(e.deltaY);
+                            } else {
+                            }
+                            console.log(e.deltaY, this.keys);
+                        },
+                        false
+                    );
+                };
+                onScrollStop(() => {
+                    this.keys.splice(0,this.keys.length);
+                    console.log(this.keys);
+                    console.log("The user has stopped scrolling");
                 });
             }
         }
@@ -317,7 +343,7 @@
                     } else {
                         this.frameTimer += deltaTime;
                     }
-                } else {     
+                } else {
                     this.speed = 0;
                     this.frameX = 0;
                 }
@@ -329,7 +355,7 @@
         let lastTime = 0;
         // player.draw(ctx);
         // player.update();
-        
+
         // const updateSpriteFrame = () => {
         //     player.constructor
         // }
@@ -345,22 +371,20 @@
         animate(0);
     };
 
-    let transformAvatar = false
+    let transformAvatar = false;
 
-    $: if ($allKidsBooksRead === true){
+    $: if ($allKidsBooksRead === true) {
         // TODO:
         // add some glittersmoke animation on top of girl when she transforms
-        transformAvatar = true
-        girlTransform = 1
+        transformAvatar = true;
+        girlTransform = 1;
         setTimeout(() => {
-            transformAvatar = false
-            avatarSpriteAnimation()
+            transformAvatar = false;
+            avatarSpriteAnimation();
         }, 2000);
     }
 
-    $: console.log(girlTransform)
-
-
+    $: console.log(girlTransform);
 
     //ver3
 
@@ -499,22 +523,22 @@
     //     }
     //     animate();
     // });
-
-
-        
 </script>
-
 
 <canvas id="canvas1" />
 <img
     class="forward"
     src="../images/transformation-spritesheet-02-1.png"
     alt="player"
-    id="playerImage"            
+    id="playerImage"
 />
 
-<div in:fade out:fade class={"glitter-smoke " + (transformAvatar ? 'visible' : '')} ></div>/> 
-
+<div
+    in:fade
+    out:fade
+    class={"glitter-smoke " + (transformAvatar ? "visible" : "")}
+/>
+/>
 
 <svelte:window
     on:keydown={(e) => (key = e.key)}
@@ -522,7 +546,7 @@
 />
 
 <style>
-    .glitter-smoke{
+    .glitter-smoke {
         background-color: #6f2dff;
         position: sticky;
         width: 200px;
@@ -535,51 +559,69 @@
         z-index: 5;
         transform: rotate(90deg) rotateY(0deg);
         /* display: none; */
-        box-shadow:
-    	0 0 50px 50px #6f2dff,  /* inner  */
-    	0 0 80px 80px #4c00ff9f, /* middle  */
-    	0 0 100px 100px #8800ff90; /* outer */
+        box-shadow: 0 0 50px 50px #6f2dff, /* inner  */ 0 0 80px 80px #4c00ff9f,
+            /* middle  */ 0 0 100px 100px #8800ff90; /* outer */
         scale: 0;
         transition: all 1s;
-		/* animation: glow 2s ease-in-out infinite alternate; */
+        /* animation: glow 2s ease-in-out infinite alternate; */
     }
 
-    .glitter-smoke.visible{
+    .glitter-smoke.visible {
         display: block;
         scale: 1.3;
         transition-duration: 1s;
         animation: shake 1s;
-		animation-iteration-count: infinite;
+        animation-iteration-count: infinite;
     }
 
     @keyframes shake {
-  0% { transform: translate(5px, 5px) rotate(0deg); }
-  10% { transform: translate(-5px, -4px) rotate(-1deg); }
-  20% { transform: translate(-7px, 2px) rotate(1deg); }
-  30% { transform: translate(5px, 3px) rotate(0deg); }
-  40% { transform: translate(1px, -1px) rotate(1deg); }
-  50% { transform: translate(-13px, 8px) rotate(-1deg); }
-  60% { transform: translate(-6px, 5px) rotate(0deg); }
-  70% { transform: translate(13px, 5px) rotate(-1deg); }
-  80% { transform: translate(-6px, -4px) rotate(1deg); }
-  90% { transform: translate(5px, 7px) rotate(0deg); }
-  100% { transform: translate(10px, -12px) rotate(-1deg); }
-}
+        0% {
+            transform: translate(5px, 5px) rotate(0deg);
+        }
+        10% {
+            transform: translate(-5px, -4px) rotate(-1deg);
+        }
+        20% {
+            transform: translate(-7px, 2px) rotate(1deg);
+        }
+        30% {
+            transform: translate(5px, 3px) rotate(0deg);
+        }
+        40% {
+            transform: translate(1px, -1px) rotate(1deg);
+        }
+        50% {
+            transform: translate(-13px, 8px) rotate(-1deg);
+        }
+        60% {
+            transform: translate(-6px, 5px) rotate(0deg);
+        }
+        70% {
+            transform: translate(13px, 5px) rotate(-1deg);
+        }
+        80% {
+            transform: translate(-6px, -4px) rotate(1deg);
+        }
+        90% {
+            transform: translate(5px, 7px) rotate(0deg);
+        }
+        100% {
+            transform: translate(10px, -12px) rotate(-1deg);
+        }
+    }
 
     @keyframes glow {
-		from{
-			box-shadow:
-    		0 0 10px 5px #6f2dff94,  /* inner  */
-    		0 0 30px 15px #4c00ff6d, /* middle  */
-    		0 0 50px 25px #8800ff6f; /* outer */
-		}
-		to{
-			box-shadow:
-    		0 0 40px 20px #6f2dff94,  /* inner  */
-    		0 0 60px 30px #4c00ff6d, /* middle  */
-    		0 0 110px 60px #8800ff6f;; /* outer  */
-		}
-	}
+        from {
+            box-shadow: 0 0 10px 5px #6f2dff94,
+                /* inner  */ 0 0 30px 15px #4c00ff6d,
+                /* middle  */ 0 0 50px 25px #8800ff6f; /* outer */
+        }
+        to {
+            box-shadow: 0 0 40px 20px #6f2dff94,
+                /* inner  */ 0 0 60px 30px #4c00ff6d,
+                /* middle  */ 0 0 110px 60px #8800ff6f; /* outer  */
+        }
+    }
 
     #canvas1 {
         /* border: 5px solid black; */
@@ -597,5 +639,4 @@
     #playerImage {
         display: none;
     }
-   
 </style>
